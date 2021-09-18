10e7d8d2d5f6e534efe164d9ea2944521aecae90
glennmatlin <glenn.matlin@gmail.com>
2021-09-18 16:49:22 -0400

    need to move hpc files on setup

--- cwcf_init.sh
+++ cwcf_init.sh
@@ -2,3 +2,5 @@ conda update --channel defaults --all --yes
 conda install -c conda-forge jupyterlab
 conda install pandas numpy
 conda install pytorch=0.4.1 cuda90 -c pytorch --yes
+
+# copy the hpc from trained_models into data OR change where it finds the hpc files
