62e82d384a582fe8175ac375e9ae9371855839b3
Glenn Scott Matlin <gmatlin3@simontam.cc.gatech.edu>
2021-09-18 16:29:54 -0400

    environment setup

---
+++ cwcf_data_init.sh
@@ -0,0 +1,3 @@
+conda update --channel defaults --all --yes
+conda install pandas numpy
+conda install pytorch torchvision cudatoolkit=10.2 -c pytorch-lts
---
+++ cwcf_init.sh
@@ -0,0 +1,4 @@
+conda update --channel defaults --all --yes
+conda install -c conda-forge jupyterlab
+conda install pandas numpy
+conda install pytorch=0.4.1 cuda90 -c pytorch --yes
